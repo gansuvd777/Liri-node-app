0f1634fdb440a18132d69b82debf2a0b419df3bb
Pearl <gansuvd777@gmail.com>
2018-11-07 12:28:03 -0600

    adding log.txt file

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-console.log('This liri bot is working');
+console.log('This liri bot is working 👌🏻');
 
 require("dotenv").config();
 
@@ -37,29 +37,28 @@ switch (action) {
     default:
         console.log("I don't understand, ask Foogle-Bot");
 };
-/* Marks review */
-// function concertThis(){
-//     console.log("Concert this");
-// }
 
 // node liri.js concert-this '<Concert name here>'
 function concert (inputs){
-    var queryUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
-    // console.log("print something");
+    var queryUrl = "https://rest.bandsintown.com/artists/" + inputs + "/events?app_id=codingbootcamp";
+    // console.log(response);
     request (queryUrl, function(error, response, body) {
-		// if (!inputs){
-        // 	inputs = 'Stronger';
-    	// }
+		if (!inputs){
+        	inputs = 'Maroon 5';
+    	}
 		if (!error && response.statusCode === 200) {
 			//limiting only first 5
-			var concertThis = JSON.parse(body).slice(0,4);
+			var concertThis = JSON.parse(body).slice(0,5);
 
 			for (var i = 0; i < concertThis.length; i++){
-
 				console.log("----------------------------------");
-				console.log(` Artist: ${inputs}\n Venue: ${concertThis[0].venue.name}\n Location: ${concertThis[0].venue.city} ${concertThis[0].venue.country}`);
-				console.log("Time"+ ": " + moment(concertThis[0].datetime, 'YYYY-MM-DDh-m-s').format('MM/DD/YYYY'));
+				console.log(` Artist: ${inputs}\n Venue: ${concertThis[i].venue.name}\n Location: ${concertThis[i].venue.city} ${concertThis[i].venue.country}\n Date of the Event: ${moment(concertThis[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a")}`);
 				console.log("----------------------------------");
+				fs.appendFile("log.txt", `----------------------------\nArtist: ${inputs}\n Venue: ${concertThis[i].venue.name}\n Location: ${concertThis[i].venue.city} ${concertThis[i].venue.country}\n Date of the Event: ${moment(concertThis[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a")}\n----------------------------`, function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
 				} 
 			}
 			else {
@@ -67,13 +66,12 @@ function concert (inputs){
 			}
 	  });
 };
-
 // node liri.js spotify-this-song '<song name here>'
 function spotify(inputs) {
 
 	var spotify = new Spotify(keys.spotify);
 		if (!inputs){
-        	inputs = 'Stronger';
+        	inputs = 'Heaven';
     	}
 		spotify.search({ type: 'track', query: inputs, limit:5 }, function(err, data) {
 			if (err){
@@ -81,15 +79,18 @@ function spotify(inputs) {
 	            return;
 	        }
 			var songInfo = data.tracks.items;
-			
-
-			console.log("----------------------------------")
+			console.log("----------------------------------");
 	        console.log(` Artist(s): ${songInfo[0].artists[0].name}\n Song Name: ${songInfo[0].name}\n Preview Link: ${songInfo[0].preview_url}\n Album: ${songInfo[0].album.name}`);
-			console.log("----------------------------------")
+			console.log("----------------------------------");
+			//Appending retrieved data to log.txt file
+			fs.appendFile("log.txt", `----------------------------\n Artist(s): ${songInfo[0].artists[0].name}\n Song Name: ${songInfo[0].name}\n Preview Link: ${songInfo[0].preview_url}\n Album: ${songInfo[0].album.name}\n----------------------------`, function (err){
+				if (err) {
+					console.log(err);
+				}
+			});
 		
 	});
 }
-
 // node liri.js movie-this '<movie name here>'
 function movie(inputs) {
 
@@ -97,14 +98,23 @@ function movie(inputs) {
 
 	request(queryUrl, function(error, response, body) {
 		if (!inputs){
-        	inputs = 'Pretty Woman';
+        	inputs = 'Mr. Nobody,';
     	}
 		if (!error && response.statusCode === 200) {
 			let movieInfo = JSON.parse(body);
 			console.log('----------------------------------');
-			console.log(` Title: ${movieInfo.Title}\n Release Year: ${movieInfo.Year}\n IMDB Rating:${movieInfo.imdbRating}\n Rotten Tomatoes Rating: ${movieInfo.Ratings[1].Value}\n Country: ${movieInfo.Country}\n Language: ${movieInfo.Language}\n Plot: ${movieInfo.Plot}\n Actors: ${movieInfo.Actors}`);
+			console.log(` Title: ${movieInfo.Title}\n Release Year: ${movieInfo.Year}\n IMDB Rating:${movieInfo.imdbRating}\n Rotten Tomatoes Rating: ${movieInfo.Ratings[0].Value}\n Country: ${movieInfo.Country}\n Language: ${movieInfo.Language}\n Plot: ${movieInfo.Plot}\n Actors: ${movieInfo.Actors}`);
 			console.log('-----------------------------------');
+			//Appending retrieved data to log.txt file
+			fs.appendFile("log.txt", `----------------------------\n Title: ${movieInfo.Title}\n Release Year: ${movieInfo.Year}\n IMDB Rating:${movieInfo.imdbRating}\n Rotten Tomatoes Rating: ${movieInfo.Ratings[0].Value}\n Country: ${movieInfo.Country}\n Language: ${movieInfo.Language}\n Plot: ${movieInfo.Plot}\n Actors: ${movieInfo.Actors}\n----------------------------`, function (err){
+				if (err) {
+					console.log(err);
+				}
+			});
 		}
+		else {
+            console.log('Error!!!!!')
+        }
 	});
 };
 // node liri.js do-what-it-says
